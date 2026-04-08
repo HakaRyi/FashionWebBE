2771d3678e2eefd8e12ff2db93553d125267dfec
dylanneedhelp <dangvanbo2004@gmail.com>
2026-04-08 15:00:38 +0700

    them lich su suggest (add 2 table)

---
+++ Domain/Entities/RecommendationHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RecommendationHistory
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int? ReferenceItemId { get; set; }
+        public string? Prompt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public virtual Account Account { get; set; } = null!;
+        public virtual Item? ReferenceItem { get; set; }
+        public virtual ICollection<RecommendationDetail> RecommendedItems { get; set; } = new List<RecommendationDetail>();
+    }
+}
