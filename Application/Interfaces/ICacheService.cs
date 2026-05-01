797a178800541110c493aa0d2aaedbba7e192777
하까리 <hxkhang1105@gmail.com>
2026-05-01 22:25:34 +0700

    optimize by Redis

---
+++ Application/Interfaces/ICacheService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICacheService
+    {
+        Task<T?> GetDataAsync<T>(string key);
+        Task SetDataAsync<T>(string key, T data, TimeSpan? absoluteExpireTime = null);
+        Task RemoveDataAsync(string key);
+        Task<T?> GetOrSetAsync<T>(string key, Func<Task<T?>> factory, TimeSpan? absoluteExpireTime = null);
+    }
+}
